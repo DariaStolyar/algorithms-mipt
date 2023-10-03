d76d3e35a5512107789941e46209a22f7ba868a7
Daria Stolyar <daria.stolyar@yandex.ru>
2023-10-03 08:56:52 +0300

    Add task 1_B

---
+++ 1_B/B.cpp
@@ -0,0 +1,35 @@
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+
+const int kPrecision = 10;
+
+int main() {
+  size_t cnt_mandrake;
+  std::cin >> cnt_mandrake;
+
+  std::vector<double> volume(cnt_mandrake);
+  for (size_t mandrake_i = 0; mandrake_i < cnt_mandrake; ++mandrake_i) {
+    std::cin >> volume[mandrake_i];
+  }
+  std::vector<double> pref_sum_logs(cnt_mandrake + 1);
+  double pref_sum_logs_i = 0;
+  pref_sum_logs[0] = 0;
+  for (size_t mandrake_i = 1; mandrake_i < cnt_mandrake + 1; ++mandrake_i) {
+    pref_sum_logs_i += log(volume[mandrake_i - 1]);
+    pref_sum_logs[mandrake_i] = pref_sum_logs_i;
+  }
+  size_t cnt_request;
+  std::cin >> cnt_request;
+  int left_i;
+  int right_i;
+  double ans_i;
+  for (size_t request_i = 0; request_i < cnt_request; ++request_i) {
+    std::cin >> left_i >> right_i;
+    ans_i = std::exp((pref_sum_logs[right_i + 1] - pref_sum_logs[left_i]) *
+                     (1. / (static_cast<double>(right_i - left_i) + 1)));
+    std::cout << std::setprecision(kPrecision) << ans_i << "\n";
+  }
+  return 0;
+}

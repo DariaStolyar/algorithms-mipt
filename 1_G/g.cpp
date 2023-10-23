bbd56720e989479276601f4d61bc4fce75e9b54b
Daria Stolyar <74820682+DariaStolyar@users.noreply.github.com>
2023-10-23 16:43:03 +0300

    Update g.cpp

--- 1_G/g.cpp
+++ 1_G/g.cpp
@@ -1,33 +1,36 @@
 #include <iostream>
 #include <vector>
 
-const int kBit = 256;
-const int kByte = 8;
-
-int main() {
-  size_t cnt_elem_list;
-  std::cin >> cnt_elem_list;
-  std::vector<unsigned long long> first_list(cnt_elem_list);
-  for (size_t i = 0; i < cnt_elem_list; ++i) {
-    std::cin >> first_list[i];
-  }
+void Sort(std::vector<unsigned long long>& list) {
+  const int kBit = 256;
+  const int kByte = 8;
   for (size_t i = 0; i < kByte; ++i) {
     std::vector<std::vector<unsigned long long>> list_byte(kBit);
-    for (size_t j = 0; j < cnt_elem_list; ++j) {
-      int j_byte = ((first_list[j] >> (i * kByte)) & (kBit - 1));
-      list_byte[j_byte].push_back(first_list[j]);
+    for (size_t j = 0; j < list.size(); ++j) {
+      int j_byte = ((list[j] >> (i * kByte)) & (kBit - 1));
+      list_byte[j_byte].push_back(list[j]);
     }
     size_t i_in_first_list = 0;
     for (size_t j = 0; j < kBit; ++j) {
       for (size_t i_in_list_byte = 0; i_in_list_byte < list_byte[j].size();
            ++i_in_list_byte) {
-        first_list[i_in_first_list] = list_byte[j][i_in_list_byte];
+        list[i_in_first_list] = list_byte[j][i_in_list_byte];
         ++i_in_first_list;
       }
     }
   }
-  for (size_t i = 0; i < cnt_elem_list; ++i) {
-    std::cout << first_list[i] << "\n";
+}
+
+int main() {
+  size_t list_length;
+  std::cin >> list_length;
+  std::vector<unsigned long long> list(list_length);
+  for (size_t i = 0; i < list_length; ++i) {
+    std::cin >> list[i];
+  }
+  Sort(list);
+  for (size_t i = 0; i < list_length; ++i) {
+    std::cout << list[i] << "\n";
   }
   return 0;
 }

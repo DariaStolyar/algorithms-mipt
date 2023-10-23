6157fb01ad4afbbe114cb3beb12bd8839aa4f7c8
Daria Stolyar <74820682+DariaStolyar@users.noreply.github.com>
2023-10-23 21:42:54 +0300

    Update h.cpp

--- 1_H/h.cpp
+++ 1_H/h.cpp
@@ -2,52 +2,49 @@
 #include <random>
 #include <vector>
 
-std::mt19937 choose_pivot(time(nullptr));
+std::pair<size_t, size_t> Partition(std::vector<int>& list, size_t begin,
+                                    size_t end) {
+  std::mt19937 choose_pivot(time(nullptr));
+  size_t index_equals = begin;
+  size_t index_larger = begin;
+  int pivot = list[begin + choose_pivot() % (end - begin)];
 
-std::pair<size_t, size_t> Partition(std::vector<int>& first_list, size_t start,
-                                    size_t finish) {
-  size_t ind_same = start;
-  size_t ind_more = start;
-  int pivot = first_list[start + choose_pivot() % (finish - start)];
-  for (size_t i = start; i < finish; ++i) {
-    if (first_list[i] < pivot) {
-      std::swap(first_list[i], first_list[ind_same]);
-      if (ind_same != ind_more) {
-        std::swap(first_list[i], first_list[ind_more]);
+  for (size_t i = begin; i < end; ++i) {
+    if (list[i] < pivot) {
+      std::swap(list[i], list[index_equals]);
+      if (index_equals != index_larger) {
+        std::swap(list[i], list[index_larger]);
       }
-      ++ind_same;
-      ++ind_more;
-    } else if (first_list[i] == pivot) {
-      std::swap(first_list[i], first_list[ind_more]);
-      ++ind_more;
+      ++index_equals;
+      ++index_larger;
+    } else if (list[i] == pivot) {
+      std::swap(list[i], list[index_larger]);
+      ++index_larger;
     }
   }
-  return {ind_same, ind_more};
+  return {index_equals, index_larger};
 }
 
-void QuickSort(std::vector<int>& first_list, size_t start, size_t finish) {
-  if (start + 1 < finish) {
-    std::pair<size_t, size_t> ind_same_and_ind_more;
-    ind_same_and_ind_more = Partition(first_list, start, finish);
-    size_t ind_same;
-    size_t ind_more;
-    ind_same = ind_same_and_ind_more.first;
-    ind_more = ind_same_and_ind_more.second;
-    QuickSort(first_list, start, ind_same);
-    QuickSort(first_list, ind_more, finish);
+void QuickSort(std::vector<int>& list, size_t begin, size_t end) {
+  if (begin + 1 < end) {
+    auto [index_equals, index_larger] = Partition(list, begin, end);
+    QuickSort(list, begin, index_equals);
+    QuickSort(list, index_larger, end);
   }
 }
 
 int main() {
-  size_t cnt_elem_list;
-  std::cin >> cnt_elem_list;
-  std::vector<int> first_list(cnt_elem_list);
-  for (size_t i = 0; i < cnt_elem_list; ++i) {
-    std::cin >> first_list[i];
+  size_t list_length;
+  std::cin >> list_length;
+  std::vector<int> list(list_length);
+  for (size_t i = 0; i < list_length; ++i) {
+    std::cin >> list[i];
   }
-  QuickSort(first_list, 0, first_list.size());
-  for (size_t i = 0; i < cnt_elem_list; ++i) {
-    std::cout << first_list[i] << " ";
+
+  QuickSort(list, 0, list.size());
+
+  for (size_t i = 0; i < list_length; ++i) {
+    std::cout << list[i] << " ";
   }
   return 0;
 }

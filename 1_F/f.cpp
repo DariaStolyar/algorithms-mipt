48833d4062ac528e7c5dd912443c9e8d70f7d613
Daria Stolyar <74820682+DariaStolyar@users.noreply.github.com>
2023-10-23 22:04:06 +0300

    Update f.cpp

--- 1_F/f.cpp
+++ 1_F/f.cpp
@@ -4,47 +4,42 @@
 #include <random>
 #include <vector>
 
-std::mt19937 choose_pivot(time(nullptr));
+std::pair<size_t, size_t> Partition(std::vector<int>& list, size_t begin,
+                                    size_t end) {
+  std::mt19937 choose_pivot(time(nullptr));
+  size_t index_equals = begin;
+  size_t index_larger = begin;
+  int pivot = list[begin + choose_pivot() % (end - begin)];
 
-std::pair<size_t, size_t> Partition(std::vector<int>& first_list, size_t start,
-                                    size_t finish) {
-  size_t ind_same = start;
-  size_t ind_more = start;
-  int pivot = first_list[start + choose_pivot() % (finish - start)];
-  for (size_t i = start; i < finish; ++i) {
-    if (first_list[i] < pivot) {
-      std::swap(first_list[i], first_list[ind_same]);
-      if (ind_same != ind_more) {
-        std::swap(first_list[i], first_list[ind_more]);
+  for (size_t i = begin; i < end; ++i) {
+    if (list[i] < pivot) {
+      std::swap(list[i], list[index_equals]);
+      if (index_equals != index_larger) {
+        std::swap(list[i], list[index_larger]);
       }
-      ++ind_same;
-      ++ind_more;
-    } else if (first_list[i] == pivot) {
-      std::swap(first_list[i], first_list[ind_more]);
-      ++ind_more;
+      ++index_equals;
+      ++index_larger;
+    } else if (list[i] == pivot) {
+      std::swap(list[i], list[index_larger]);
+      ++index_larger;
     }
   }
-  return {ind_same, ind_more};
+  return {index_equals, index_larger};
 }
 
-int QuickSelect(std::vector<int>& first_list, size_t start, size_t finish,
-                size_t stat) {
-  if (start + 1 >= finish) {
-    return first_list[start];
+int QuickSelect(std::vector<int>& list, size_t begin, size_t end,
+                size_t statistic) {
+  if (begin + 1 >= end) {
+    return list[begin];
   }
-  std::pair<size_t, size_t> ind_same_and_ind_more;
-  ind_same_and_ind_more = Partition(first_list, start, finish);
-  size_t ind_same;
-  size_t ind_more;
-  ind_same = ind_same_and_ind_more.first;
-  ind_more = ind_same_and_ind_more.second;
-  if (stat < ind_same) {
-    return QuickSelect(first_list, start, ind_same, stat);
+  auto [index_equals, index_larger] = Partition(list, begin, end);
+  if (statistic < index_equals) {
+    return QuickSelect(list, begin, index_equals, statistic);
   }
-  if (stat < ind_more) {
-    return first_list[ind_same];
+  if (statistic < index_larger) {
+    return list[index_equals];
   }
-  return QuickSelect(first_list, ind_more, finish, stat);
+  return QuickSelect(list, index_larger, end, statistic);
 }
 
 int main() {
@@ -52,11 +47,11 @@ int main() {
   const int kNum123 = 123;
   const int kNum45 = 45;
   const int kNum1e7 = 1e7 + 4321;
-  size_t cnt_elem_list;
-  size_t ord_stat;
-  std::cin >> cnt_elem_list >> ord_stat;
-  std::vector<int> first_list(cnt_elem_list);
-  for (size_t i = 0; i < cnt_elem_list; ++i) {
+  size_t list_length;
+  size_t statistic;
+  std::cin >> list_length >> statistic;
+  std::vector<int> first_list(list_length);
+  for (size_t i = 0; i < list_length; ++i) {
     if (i < kElem) {
       std::cin >> first_list[i];
     } else {
@@ -64,6 +59,6 @@ int main() {
           (first_list[i - 1] * kNum123 + first_list[i - 2] * kNum45) % kNum1e7;
     }
   }
-  std::cout << QuickSelect(first_list, 0, first_list.size(), ord_stat - 1);
+  std::cout << QuickSelect(first_list, 0, first_list.size(), statistic - 1);
   return 0;
 }

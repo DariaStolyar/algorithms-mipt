f211b860e1510194a88894f70f69efbe7df89434
Daria Stolyar <74820682+DariaStolyar@users.noreply.github.com>
2023-10-26 00:48:42 +0300

    Update B.cpp

--- 1_B/B.cpp
+++ 1_B/B.cpp
@@ -15,23 +15,23 @@ int main() {
   for (size_t mandrake_i = 0; mandrake_i < count_mandrake; ++mandrake_i) {
     std::cin >> volume[mandrake_i];
   }
-  std::vector<double> pref_sum_logs(count_mandrake + 1);
-  double pref_sum_logs_i = 0;
-  pref_sum_logs[0] = 0;
+  std::vector<double> prefix_sum_logs(count_mandrake + 1);
+  double prefix_sum_logs_i = 0;
+  prefix_sum_logs[0] = 0;
   for (size_t mandrake_i = 1; mandrake_i < count_mandrake + 1; ++mandrake_i) {
-    pref_sum_logs_i += log(volume[mandrake_i - 1]);
-    pref_sum_logs[mandrake_i] = pref_sum_logs_i;
+    prefix_sum_logs_i += log(volume[mandrake_i - 1]);
+    prefix_sum_logs[mandrake_i] = prefix_sum_logs_i;
   }
-  size_t cnt_request;
-  std::cin >> cnt_request;
-  for (size_t request_i = 0; request_i < cnt_request; ++request_i) {
-    int left_i;
-    int right_i;
+  size_t count_request;
+  std::cin >> count_request;
+  for (size_t request_i = 0; request_i < count_request; ++request_i) {
+    size_t left;
+    size_t right;
     double geometric_mean;
-    std::cin >> left_i >> right_i;
+    std::cin >> left >> right;
     geometric_mean =
-        std::exp((pref_sum_logs[right_i + 1] - pref_sum_logs[left_i]) *
-                 (1. / (static_cast<double>(right_i - left_i) + 1)));
+        std::exp((prefix_sum_logs[right + 1] - prefix_sum_logs[left]) *
+                 (1. / (static_cast<double>(right - left) + 1)));
     std::cout << std::setprecision(kPrecision) << geometric_mean << "\n";
   }
   return 0;

534588599171039241d46c81c326aee9d26a2c16
Daria Stolyar <74820682+DariaStolyar@users.noreply.github.com>
2023-11-01 23:27:49 +0300

    Update g.cpp

--- 1_G/g.cpp
+++ 1_G/g.cpp
@@ -1,36 +1,46 @@
+// В задаче необходимо отсортировать массив поразрядной сортировкой по байтам
+
 #include <iostream>
 #include <vector>
 
-void Sort(std::vector<unsigned long long>& list) {
+int ByteOfNumber(unsigned long long number, size_t byte) {
+  const int kBit = 256;
+  const int kByte = 8;
+  return (number >> (byte * kByte)) & (kBit - 1);
+}
+
+void LSDSort(std::vector<unsigned long long>& array) {
   const int kBit = 256;
   const int kByte = 8;
   for (size_t i = 0; i < kByte; ++i) {
-    std::vector<std::vector<unsigned long long>> list_byte(kBit);
-    for (size_t j = 0; j < list.size(); ++j) {
-      int j_byte = ((list[j] >> (i * kByte)) & (kBit - 1));
-      list_byte[j_byte].push_back(list[j]);
+    std::vector<unsigned long long> vector_byte(kBit, 0);
+    std::vector<unsigned long long> new_array(array.size(), 0);
+    std::vector<unsigned long long> array_byte(array.size(), 0);
+    for (size_t j = 0; j < array.size(); ++j) {
+      array_byte[j] = ByteOfNumber(array[j], i);
+      ++vector_byte[array_byte[j]];
     }
-    size_t i_in_first_list = 0;
-    for (size_t j = 0; j < kBit; ++j) {
-      for (size_t i_in_list_byte = 0; i_in_list_byte < list_byte[j].size();
-           ++i_in_list_byte) {
-        list[i_in_first_list] = list_byte[j][i_in_list_byte];
-        ++i_in_first_list;
-      }
+    for (size_t j = 1; j < kBit; ++j) {
+      vector_byte[j] += vector_byte[j - 1];
     }
+    for (int j = array.size() - 1; j >= 0; --j) {
+      new_array[vector_byte[array_byte[j]] - 1] = array[j];
+      --vector_byte[array_byte[j]];
+    }
+    array = new_array;
   }
 }
 
 int main() {
-  size_t list_length;
-  std::cin >> list_length;
-  std::vector<unsigned long long> list(list_length);
-  for (size_t i = 0; i < list_length; ++i) {
-    std::cin >> list[i];
+  size_t array_length;
+  std::cin >> array_length;
+  std::vector<unsigned long long> array(array_length);
+  for (size_t i = 0; i < array_length; ++i) {
+    std::cin >> array[i];
   }
-  Sort(list);
-  for (size_t i = 0; i < list_length; ++i) {
-    std::cout << list[i] << "\n";
+  LSDSort(array);
+  for (size_t i = 0; i < array_length; ++i) {
+    std::cout << array[i] << "\n";
   }
   return 0;
 }

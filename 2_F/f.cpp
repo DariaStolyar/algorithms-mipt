25ad1c0f3cfd2bc4d15a696fba98ff06a8d5fe43
Daria Stolyar <74820682+DariaStolyar@users.noreply.github.com>
2023-12-13 23:03:32 +0300

    Update f.cpp

--- 2_F/f.cpp
+++ 2_F/f.cpp
@@ -2,21 +2,31 @@
 #include <string>
 #include <vector>
 
-struct Heap {
+class Heap {
+ public:
   std::vector<long long> binary_heap;
   long long last_index_of_heap = 0;
   std::vector<long long> heap_to_queries;
   std::vector<long long> queries;
+  Heap() {
+    const int kNumber = 10e6;
+    queries.resize(kNumber + 1, 0);
+    heap_to_queries.resize(kNumber + 1, 0);
+    binary_heap.push_back(0);
+  }
+  void Swap(long long first_index, long long second_index) {
+    std::swap(binary_heap[first_index], binary_heap[second_index]);
+    std::swap(heap_to_queries[first_index], heap_to_queries[second_index]);
+    queries[heap_to_queries[second_index]] = second_index;
+    queries[heap_to_queries[first_index]] = first_index;
+  }
   void SiftUp(long long index_element) {
     if (index_element == 1) {
       return;
     }
     long long parent = index_element / 2;
     if (binary_heap[index_element] < binary_heap[parent]) {
-      std::swap(binary_heap[index_element], binary_heap[parent]);
-      std::swap(heap_to_queries[index_element], heap_to_queries[parent]);
-      queries[heap_to_queries[parent]] = parent;
-      queries[heap_to_queries[index_element]] = index_element;
+      Swap(index_element, parent);
       SiftUp(parent);
     }
   }
@@ -31,16 +41,13 @@ struct Heap {
       child += 1;
     }
     if (binary_heap[child] < binary_heap[index_element]) {
-      std::swap(binary_heap[index_element], binary_heap[child]);
-      std::swap(heap_to_queries[index_element], heap_to_queries[child]);
-      queries[heap_to_queries[child]] = child;
-      queries[heap_to_queries[index_element]] = index_element;
+      Swap(child, index_element);
       SiftDown(child);
     }
   }
-  void Insert(long long index_query, long long new_element) {
+  void Insert(size_t index_query, long long new_element) {
     binary_heap.push_back(new_element);
-    last_index_of_heap += 1;
+    ++last_index_of_heap;
     heap_to_queries[last_index_of_heap] = index_query;
     queries[index_query] = last_index_of_heap;
     SiftUp(last_index_of_heap);
@@ -65,7 +72,7 @@ struct Heap {
   }
 };
 
-void Speed() {
+void BoostIO() {
   std::ios::sync_with_stdio(false);
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(0);
@@ -73,14 +80,10 @@ void Speed() {
 }
 
 int main() {
-  Speed();
-  const int kNumber = 10e6;
+  BoostIO();
   size_t count_query;
   std::cin >> count_query;
   Heap heap;
-  heap.queries.resize(kNumber + 1, 0);
-  heap.heap_to_queries.resize(kNumber + 1, 0);
-  heap.binary_heap.push_back(0);
   for (size_t i = 0; i < count_query; ++i) {
     std::string type_query;
     std::cin >> type_query;
@@ -90,7 +93,7 @@ int main() {
     if (type_query == "insert") {
       int new_element;
       std::cin >> new_element;
-      heap.Insert(static_cast<long long>(i + 1), new_element);
+      heap.Insert(i + 1, new_element);
     }
     if (type_query == "extractMin") {
       heap.ExtractMin();

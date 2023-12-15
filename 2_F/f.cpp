a5bcdbf37c1c458c2da980bdde66d1216f4b6177
Daria Stolyar <74820682+DariaStolyar@users.noreply.github.com>
2023-12-15 21:14:00 +0300

    Update f.cpp

--- 2_F/f.cpp
+++ 2_F/f.cpp
@@ -1,75 +1,84 @@
+// В задаче необходимо реализовать кучу на минимум
+// с операцией уменьшения числа по номеру запроса
+
 #include <iostream>
 #include <string>
 #include <vector>
 
 class Heap {
- public:
-  std::vector<long long> binary_heap;
-  size_t last_index_of_heap = 0;
-  std::vector<long long> heap_to_queries;
-  std::vector<long long> queries;
-  Heap() {
-    const int kNumber = 10e6;
-    queries.resize(kNumber + 1, 0);
-    heap_to_queries.resize(kNumber + 1, 0);
-    binary_heap.push_back(0);
-  }
+ private:
+  std::vector<long long> heap_;
+  size_t last_index_ = 0;
+  std::vector<size_t> to_queries_;
+  std::vector<size_t> queries_;
+
   void Swap(size_t first_index, size_t second_index) {
-    std::swap(binary_heap[first_index], binary_heap[second_index]);
-    std::swap(heap_to_queries[first_index], heap_to_queries[second_index]);
-    queries[heap_to_queries[second_index]] = second_index;
-    queries[heap_to_queries[first_index]] = first_index;
+    std::swap(heap_[first_index], heap_[second_index]);
+    std::swap(to_queries_[first_index], to_queries_[second_index]);
+    queries_[to_queries_[second_index]] = second_index;
+    queries_[to_queries_[first_index]] = first_index;
   }
+
   void SiftUp(size_t index_element) {
     if (index_element == 1) {
       return;
     }
-    long long parent = index_element / 2;
-    if (binary_heap[index_element] < binary_heap[parent]) {
+    size_t parent = index_element / 2;
+    if (heap_[index_element] < heap_[parent]) {
       Swap(index_element, parent);
       SiftUp(parent);
     }
   }
+
   void SiftDown(size_t index_element) {
-    if (2 * index_element > last_index_of_heap) {
+    if (2 * index_element > last_index_) {
       return;
     }
-    long long last_element = last_index_of_heap;
-    long long child = 2 * index_element;
-    if (child + 1 <= last_element &&
-        binary_heap[child + 1] < binary_heap[child]) {
+    size_t last_element = last_index_;
+    size_t child = 2 * index_element;
+    if (child + 1 <= last_element && heap_[child + 1] < heap_[child]) {
       child += 1;
     }
-    if (binary_heap[child] < binary_heap[index_element]) {
+    if (heap_[child] < heap_[index_element]) {
       Swap(child, index_element);
       SiftDown(child);
     }
   }
+
+ public:
+  Heap(size_t size) {
+    queries_.resize(size + 1, 0);
+    to_queries_.resize(size + 1, 0);
+    heap_.push_back(0);
+  }
+
   void Insert(size_t index_query, long long new_element) {
-    binary_heap.push_back(new_element);
-    ++last_index_of_heap;
-    heap_to_queries[last_index_of_heap] = index_query;
-    queries[index_query] = last_index_of_heap;
-    SiftUp(last_index_of_heap);
+    heap_.push_back(new_element);
+    ++last_index_;
+    to_queries_[last_index_] = index_query;
+    queries_[index_query] = last_index_;
+    SiftUp(last_index_);
   }
 
   void ExtractMin() {
-    long long last = last_index_of_heap;
+    size_t last = last_index_;
     size_t first = 1;
-    std::swap(binary_heap[first], binary_heap[last]);
-    std::swap(heap_to_queries[first], heap_to_queries[last]);
-    queries[heap_to_queries[last]] = last;
-    queries[heap_to_queries[first]] = first;
-    binary_heap.pop_back();
-    last_index_of_heap -= 1;
+    std::swap(heap_[first], heap_[last]);
+    std::swap(to_queries_[first], to_queries_[last]);
+    queries_[to_queries_[last]] = last;
+    queries_[to_queries_[first]] = first;
+    heap_.pop_back();
+    last_index_ -= 1;
     SiftDown(first);
   }
 
   void DecreaseKey(size_t index_query, long long delta) {
-    long long index_element = queries[index_query];
-    binary_heap[index_element] -= delta;
+    size_t index_element = queries_[index_query];
+    heap_[index_element] -= delta;
     SiftUp(index_element);
   }
+
+  long long GetMin() { return heap_[1]; }
 };
 
 void BoostIO() {
@@ -83,23 +92,28 @@ int main() {
   BoostIO();
   size_t count_query;
   std::cin >> count_query;
-  Heap heap;
+  Heap heap(count_query);
+ 
   for (size_t i = 0; i < count_query; ++i) {
     std::string type_query;
     std::cin >> type_query;
+   
     if (type_query == "getMin") {
-      std::cout << heap.binary_heap[1] << "\n";
+      std::cout << heap.GetMin() << "\n";
     }
+   
     if (type_query == "insert") {
       int new_element;
       std::cin >> new_element;
       heap.Insert(i + 1, new_element);
     }
+   
     if (type_query == "extractMin") {
       heap.ExtractMin();
     }
+   
     if (type_query == "decreaseKey") {
-      long long index_query;
+      size_t index_query;
       long long delta;
       std::cin >> index_query >> delta;
       heap.DecreaseKey(index_query, delta);

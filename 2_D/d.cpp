71668d79fd5fb462200848133f2551ccb51bb88f
Daria Stolyar <74820682+DariaStolyar@users.noreply.github.com>
2023-12-15 21:48:05 +0300

    Update d.cpp

--- 2_D/d.cpp
+++ 2_D/d.cpp
@@ -5,74 +5,74 @@
 #include <string>
 
 class Hat {
-private:
-    std::stack<int> begin_queue_;
-    std::stack<int> end_queue_;
-    std::stack<int> end_queue_min_;
-    std::stack<int> begin_queue_min_;
+ private:
+  std::stack<int> begin_queue_;
+  std::stack<int> end_queue_;
+  std::stack<int> end_queue_min_;
+  std::stack<int> begin_queue_min_;
 
-    void Correction() {
-      while (!end_queue_.empty()) {
-        begin_queue_.push(end_queue_.top());
-        if (begin_queue_min_.empty()) {
-          begin_queue_min_.push(end_queue_.top());
-        } else {
-          begin_queue_min_.push(
-                  std::min(begin_queue_min_.top(), end_queue_.top()));
-        }
-        end_queue_.pop();
-        end_queue_min_.pop();
-      }
-    }
-
-public:
-    bool HaveFront() { return !(end_queue_.empty() && begin_queue_.empty()); }
-
-    int Front() {
-      if (begin_queue_.empty()) {
-        Correction();
-      }
-      return begin_queue_.top();
-    }
-
-    int Min() {
-      if (end_queue_min_.empty()) {
-        return begin_queue_min_.top();
-      } else if (begin_queue_min_.empty()) {
-        return end_queue_min_.top();
+  void Correction() {
+    while (!end_queue_.empty()) {
+      begin_queue_.push(end_queue_.top());
+      if (begin_queue_min_.empty()) {
+        begin_queue_min_.push(end_queue_.top());
       } else {
-        return std::min(begin_queue_min_.top(), end_queue_min_.top());
+        begin_queue_min_.push(
+            std::min(begin_queue_min_.top(), end_queue_.top()));
       }
+      end_queue_.pop();
+      end_queue_min_.pop();
     }
+  }
 
-    void ClearHat() {
-      size_t size_begin = begin_queue_.size();
-      size_t size_end = end_queue_.size();
-      for (size_t j = 0; j < size_begin; ++j) {
-        begin_queue_.pop();
-        begin_queue_min_.pop();
-      }
-      for (size_t j = 0; j < size_end; ++j) {
-        end_queue_.pop();
-        end_queue_min_.pop();
-      }
+ public:
+  bool HaveFront() { return !(end_queue_.empty() && begin_queue_.empty()); }
+
+  int Front() {
+    if (begin_queue_.empty()) {
+      Correction();
     }
+    return begin_queue_.top();
+  }
 
-    void Enqueue(int new_element) {
-      end_queue_.push(new_element);
-      if (end_queue_min_.empty()) {
-        end_queue_min_.push(new_element);
-      } else {
-        end_queue_min_.push(std::min(end_queue_min_.top(), new_element));
-      }
+  int Min() {
+    if (end_queue_min_.empty()) {
+      return begin_queue_min_.top();
     }
+    if (begin_queue_min_.empty()) {
+      return end_queue_min_.top();
+    }
+    return std::min(begin_queue_min_.top(), end_queue_min_.top());
+  }
 
-    size_t Size() { return begin_queue_.size() + end_queue_.size(); }
-
-    void Dequeue() {
+  void ClearHat() {
+    size_t size_begin = begin_queue_.size();
+    size_t size_end = end_queue_.size();
+    for (size_t j = 0; j < size_begin; ++j) {
       begin_queue_.pop();
       begin_queue_min_.pop();
     }
+    for (size_t j = 0; j < size_end; ++j) {
+      end_queue_.pop();
+      end_queue_min_.pop();
+    }
+  }
+
+  void Enqueue(int new_element) {
+    end_queue_.push(new_element);
+    if (end_queue_min_.empty()) {
+      end_queue_min_.push(new_element);
+    } else {
+      end_queue_min_.push(std::min(end_queue_min_.top(), new_element));
+    }
+  }
+
+  size_t Size() { return begin_queue_.size() + end_queue_.size(); }
+
+  void Dequeue() {
+    begin_queue_.pop();
+    begin_queue_min_.pop();
+  }
 };
 
 int main() {
